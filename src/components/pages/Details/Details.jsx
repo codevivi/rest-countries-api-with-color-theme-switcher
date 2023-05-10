7407a265fbc71f0c872deb10d98e86a0eceb0aa6
Vilma <vilmail@protonmail.com>
2023-05-10 22:14:55 +0300

    working details, but need styles

--- src/components/pages/Details/Details.jsx
+++ src/components/pages/Details/Details.jsx
@@ -1,4 +1,4 @@
-import { Link, redirect, useParams } from "react-router-dom";
+import { Link, redirect, useNavigate, useParams } from "react-router-dom";
 import { HiArrowLongLeft } from "react-icons/hi2";
 import { useContext, useEffect, useState } from "react";
 import { GlobalCtx } from "../../../context/GlobalCtx";
@@ -6,14 +6,17 @@ import "../../../scss/_buttons.scss";
 import "./_details.scss";
 function Details() {
   const { countryName } = useParams();
-  const { allCountries } = useContext(GlobalCtx);
+  const { allCountries, codedNames } = useContext(GlobalCtx);
   const [country, setCountry] = useState(null);
+  const navigate = useNavigate();
+
   useEffect(() => {
     if (allCountries === null) {
       return;
     }
-    setCountry(allCountries.get(countryName));
-  }, [allCountries, countryName]);
+    let tempCountry = allCountries.get(countryName);
+    setCountry({ ...tempCountry, borders: tempCountry.borders.map((code) => codedNames.get(code)) });
+  }, [allCountries, countryName, codedNames]);
 
   if (allCountries === null) {
     redirect("/");
@@ -25,12 +28,60 @@ function Details() {
 
   return (
     <div className="details">
-      <Link to="/" className="bg button large">
+      <button onClick={() => navigate(-1)} className="bg button large">
         <HiArrowLongLeft />
         <span>Back</span>
-      </Link>
+      </button>
 
-      <div>Details {country.name}</div>
+      <div className="flex">
+        <div className="flag-box">
+          <img src={country.flagUrl} alt={country.flagAlt} />
+        </div>
+        <div className="content-box">
+          <h2>{country.name}</h2>
+          <div className="stats">
+            <p>
+              <strong>Native Name: </strong>
+              <span>{country.nativeName}</span>
+            </p>
+            <p>
+              <strong>Population: </strong>
+              <span>{country.population}</span>
+            </p>
+            <p>
+              <strong>Region: </strong>
+              <span>{country.region}</span>
+            </p>
+
+            <p>
+              <strong>Sub Region: </strong>
+              <span>{country.subRegion}</span>
+            </p>
+            <p>
+              <strong>Capital: </strong>
+              <span>{country.capital}</span>
+            </p>
+
+            <p>
+              <strong>Top Level Domain: </strong>
+              <span>{country.topLevelDomain}</span>
+            </p>
+
+            <p>
+              <strong>Languages: </strong>
+              <span>{country.languages}</span>
+            </p>
+            <div>
+              <strong>Border Countries: </strong>
+              {country.borders.map((borderCountryName) => (
+                <Link key={borderCountryName} to={"/details/" + borderCountryName} className="button bg">
+                  {borderCountryName}
+                </Link>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
